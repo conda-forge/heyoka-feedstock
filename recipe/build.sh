a1689f83e2a9ec0fd7fa3654b5c7b2814b7f41f9
Francesco Biscani <bluescarni@gmail.com>
2021-09-28 17:24:08 +0200

    Bump to 0.15.

--- recipe/build.sh
+++ recipe/build.sh
@@ -5,10 +5,16 @@ if [[ "$target_platform" == osx-* ]]; then
     # Workaround for missing C++17 feature when building the tests.
     # Also, workaround for compile issue on older OSX SDKs.
     export CXXFLAGS="$CXXFLAGS -DCATCH_CONFIG_NO_CPP17_UNCAUGHT_EXCEPTIONS -D_LIBCPP_DISABLE_AVAILABILITY"
-elif [[ "$target_platform" == linux-aarch64 ||  "$target_platform" == linux-ppc64le ]]; then
+elif [[ "$target_platform" == linux-aarch64 ]]; then
     export ENABLE_MPPP=no
 else
     export ENABLE_MPPP=yes
+fi
+
+if [[ "$target_platform" == linux-ppc64le ]]; then
+    export ENABLE_IPO=no
+else
+    export ENABLE_IPO=yes
 fi
 
 mkdir build
@@ -21,7 +27,7 @@ cmake ${CMAKE_ARGS} \
     -DHEYOKA_WITH_MPPP=$ENABLE_MPPP \
     -DHEYOKA_BUILD_TESTS=yes \
     -DHEYOKA_WITH_SLEEF=yes \
-    -DHEYOKA_ENABLE_IPO=yes \
+    -DHEYOKA_ENABLE_IPO=$ENABLE_IPO \
     -DBoost_NO_BOOST_CMAKE=ON \
     -DHEYOKA_INSTALL_LIBDIR=lib \
     ..
